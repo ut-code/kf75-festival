2d84b9d4d3bfba5b9fbd514ab08131feea40d1e2
aster <137767097+aster-void@users.noreply.github.com>
2024-09-15 18:26:35 +0900

    add flake and direnv and justfile

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_22
+        ];
+      };
+    });
+}
---
+++ justfile
@@ -0,0 +1,14 @@
+default: start
+
+start:
+    npm run start
+
+watch:
+    echo "Unknown command"
+    exit 1
+
+build:
+    npm run build
+
+serve:
+    npm run serve
