a2af8fbeab5d8f1ae60fa3377c240575e9bc55d6
aster <137767097+aster-void@users.noreply.github.com>
2024-11-16 16:20:43 +0900

    fix the collection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,39 +2,61 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import Heading from "@theme/Heading";
 
-const activities = [
-  // QR Code: temporary project
+type TargetAge = "どなたでも" | "小中学生以上におすすめ" | "大人向け";
+const timeEst = {
+  short: "5分程度",
+  med: "15分ほど",
+  long: "30分〜",
+};
+type TimeEst = "short" | "med" | "long";
+
+type Activitiy = {
+  title: string;
+  image: {
+    src: string;
+    alt: string;
+  };
+  description: string;
+  to: string; // URL
+  isNew: boolean;
+  target: TargetAge;
+  takes: TimeEst;
+};
+
+const activities: Activitiy[] = [
   {
     title: "AIオセロ",
-    coverImage: {
-      // src: "img/hoge.png",
+    image: {
+      src: "img/hoge.png",
       alt: "オセロのイメージ",
     },
-    description:
-      // prettier-ignore
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore corporis enim accusamus. Et voluptates, atque consequuntur, dolores dicta quibusdam reprehenderit aliquam veritatis unde sit ad. Consequatur quis ea autem velit.
-      </p>,
-    linkTo: "/ai-othello",
+    description: `
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+				corporis enim accusamus. Et voluptates, atque consequuntur, dolores
+				dicta quibusdam reprehenderit aliquam veritatis unde sit ad. Consequatur
+				quis ea autem velit.
+				`,
+    to: "/ai-othello",
     isNew: true,
-    targetAge: "どなたでも",
-    requiredTime: "30分～",
+    target: "どなたでも",
+    takes: "long",
   },
   {
     title: "ブロックプログラミングで迷路",
-    coverImage: {
+    image: {
       src: "https://source.unsplash.com/hLvQ4-QEBAE/800x300",
       alt: "迷路のイメージ",
     },
-    description:
-      // prettier-ignore
-      <p>
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos sint est omnis incidunt harum, maiores accusantium reiciendis id error nobis! Molestiae odit dignissimos illo nisi aperiam quod repellat placeat voluptas.
-      </p>,
-    linkTo: "/mage",
+    description: `
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos sint est
+				omnis incidunt harum, maiores accusantium reiciendis id error nobis!
+				Molestiae odit dignissimos illo nisi aperiam quod repellat placeat
+				voluptas.
+			`,
+    to: "/mage",
     isNew: false,
-    targetAge: "小学生以上におすすめ",
-    requiredTime: "10分～",
+    target: "小中学生以上におすすめ",
+    takes: "med",
   },
   /*
   {
@@ -56,72 +78,72 @@ Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos sint est omnis inc
   */
   {
     title: "麻雀",
-    coverImage: {
-      // src: "img/piyo.png",
+    image: {
+      src: "img/piyo.png",
       alt: "麻雀のイメージ",
     },
-    description:
-      // prettier-ignore
-      <p>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis eum quo cum voluptatem iure ab consectetur accusantium porro vel. Alias debitis minus similique blanditiis velit quo, quia enim quas aliquam!
-      </p>,
-    linkTo: "/mahjong",
+    description: `
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis eum
+				quo cum voluptatem iure ab consectetur accusantium porro vel. Alias
+				debitis minus similique blanditiis velit quo, quia enim quas aliquam!
+			`,
+    to: "/mahjong",
     isNew: true,
-    targetAge: "中学生以上におすすめ",
-    requiredTime: "10秒〜",
+    target: "小中学生以上におすすめ",
+    takes: "short",
   },
-  // permanent project:
   {
     title: "Blosite",
-    coverImage: {
-      // src: "img/piyo.png",
+    image: {
+      src: "img/piyo.png",
       alt: "blosite のイメージ",
     },
-    description:
-      // prettier-ignore
-      <p>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae consequuntur pariatur quam nihil quasi a tempora cum magnam natus labore velit ipsum provident, voluptas, expedita hic neque repellendus ipsam sapiente?
-      </p>,
-    linkTo: "/blosite",
+    description: `
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+				consequuntur pariatur quam nihil quasi a tempora cum magnam natus labore
+				velit ipsum provident, voluptas, expedita hic neque repellendus ipsam
+				sapiente?
+			`,
+    to: "/blosite",
     isNew: true,
-    targetAge: "中学生以上におすすめ",
-    requiredTime: "10分～",
+    target: "小中学生以上におすすめ",
+    takes: "med",
   },
   {
     title: "ハッカーになろう",
-    coverImage: {
-      // src: "img/piyo.png",
+    image: {
+      src: "img/piyo.png",
       alt: "セキュリティのイメージ",
     },
-    description:
-      // prettier-ignore
-      <p>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae nisi similique recusandae maxime suscipit explicabo ex inventore. Laborum voluptates assumenda accusantium consequatur veniam ab amet, temporibus repudiandae distinctio, dignissimos rerum?
-      </p>,
-    linkTo: "/security",
+    description: `
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+				nisi similique recusandae maxime suscipit explicabo ex inventore.
+				Laborum voluptates assumenda accusantium consequatur veniam ab amet,
+				temporibus repudiandae distinctio, dignissimos rerum?
+				`,
+    to: "/security",
     isNew: false,
-    targetAge: "高校生以上におすすめ",
-    requiredTime: "5分～",
+    target: "大人向け",
+    takes: "short", // 本当に？
   },
   {
     title: "競馬AI",
-    coverImage: {
-      // src: "img/piyo.png",
+    image: {
+      src: "img/piyo.png",
       alt: "競馬のイメージ",
     },
-    description:
-      // prettier-ignore
-      <p>
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatem quo minima ratione, aperiam iste aliquid! Dolor fugiat rerum, ut est itaque ipsa similique. Animi unde praesentium quis error eum.
-      </p>,
-    linkTo: "/horse-racing",
+    description: `
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+				voluptatem quo minima ratione, aperiam iste aliquid! Dolor fugiat rerum,
+				ut est itaque ipsa similique. Animi unde praesentium quis error eum.
+			`,
+    to: "/horse-racing",
     isNew: true,
-    targetAge: "大学生以上におすすめ",
-    requiredTime: "5分～",
+    target: "大人向け",
+    takes: "short",
   },
 ];
 
-// prettier-ignore
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -148,8 +170,8 @@ export default function Home(): JSX.Element {
                 <div className="card__image">
                   <img
                     style={{ width: "100%" }}
-                    src={activity.coverImage.src}
-                    alt={activity.coverImage.alt}
+                    src={activity.image.src}
+                    alt={activity.image.alt}
                   />
                 </div>
                 <div className="card__body">
@@ -157,13 +179,13 @@ export default function Home(): JSX.Element {
                   {activity.isNew && (
                     <h3 className="badge badge--success">NEW</h3>
                   )}
-                  <h3 className="badge badge--primary">{activity.targetAge}</h3>
-                  <h3 className="badge badge--info">{activity.requiredTime}</h3>
-                  {activity.description}
+                  <h3 className="badge badge--primary">{activity.target}</h3>
+                  <h3 className="badge badge--info">{activity.takes}</h3>
+                  <p>{activity.description}</p>
                 </div>
                 <div className="card__footer">
                   <a
-                    href={activity.linkTo}
+                    href={activity.to}
                     target="_blank"
                     rel="noreferrer"
                     className="button button--primary button--block"
