d12c3dd7116f8582d6a86856f50d729d8ee0613a
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-11-19 19:32:55 +0900

    投票の文章を変更

--- src/pages/vote/index.tsx
+++ src/pages/vote/index.tsx
@@ -37,11 +37,13 @@ export default function Home(): JSX.Element {
       <main className="container padding-vert--lg">
         <p>
           本日は企画にお越しいただきありがとうございます。次のQRコードまたは
-          <a href="/prix">こちらのリンク</a>
-          から投票をお願いしております。1分ほどで完了しますので、次の動画を参考に投票をお願いします。
+          <a href="https://www.komabasai.net/75/visitor/project/357">
+            こちらのリンク
+          </a>
+          から駒場祭グランプリへの投票をお願いしております。1分ほどで完了しますので、次の動画を参考に投票をお願いします！
         </p>
         <div className={styles.container}>
-          <QRCode />
+          <img src="img/kf75QR.svg"></img>
           <video
             src={howToVoteVideo}
             controls
